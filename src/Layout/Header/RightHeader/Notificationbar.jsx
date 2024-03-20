0bcaa8462e233dc325c5d7e233b3bbb458b5ee77
Syed M.Abid Zaidi <zaidiabid51472@gmail.com>
2024-03-20 17:06:55 +0500

    Deal inquiries

--- src/Layout/Header/RightHeader/Notificationbar.jsx
+++ src/Layout/Header/RightHeader/Notificationbar.jsx
@@ -16,32 +16,10 @@ const Notificationbar = () => {
         <ul>
           <li className='b-l-primary border-4'>
             <p>
-              {DeliveryProcessing} <span className='font-danger'>{'10 min.'}</span>
+              {"Last Transaction "} <span className='font-danger'>{'10 min ago'}</span>
             </p>
           </li>
-          <li className='b-l-success border-4'>
-            <p>
-              {OrderComplete}
-              <span className='font-success'>{'1 hr'}</span>
-            </p>
-          </li>
-          <li className='b-l-info border-4'>
-            <p>
-              {TicketsGenerated}
-              <span className='font-info'>{'3 hr'}</span>
-            </p>
-          </li>
-          <li className='b-l-warning border-4'>
-            <p>
-              {DeliveryComplete}
-              <span className='font-warning'>{'6 hr'}</span>
-            </p>
-          </li>
-          <li>
-            <a className='f-w-700' href='#javascript'>
-              {CHECKALL}
-            </a>
-          </li>
+          
         </ul>
       </div>
     </li>
--- src/myPages/DealsSection/comp/Queries.jsx
+++ src/myPages/DealsSection/comp/Queries.jsx
@@ -1,73 +1,109 @@
-import React, { Fragment, useState } from "react";
-import {
-  Button,
-  Col,
-  Media,
-  Row,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  InputGroup,
-  Input,
-} from "reactstrap";
-import {
-  Breadcrumbs,
-  Btn,
-  H4,
-  H6,
-  Image,
-  LI,
-  P,
-  UL,
-} from "../../../AbstractElements";
+import React, { Fragment, useState, useRef, useEffect } from "react";
+import { Button, Col, Media, Row, InputGroup, Form, Input } from "reactstrap";
+import { Breadcrumbs, H6, Image, P, UL, LI, Btn } from "../../../AbstractElements";
 import SimpleMdeReact from "react-simplemde-editor";
-
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; // Import Quill styles
+import { EditorChange } from 'quill';
 const Queries = () => {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const commentContainerRef = useRef(null);
+
+  const handleSendComment = (comment) => {
+    if (commentContainerRef.current) {
+      // Implement logic to append the new comment to the existing comments
+      console.log("Sending comment:", comment);
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, []);
+
+  const scrollToBottom = () => {
+    if (commentContainerRef.current) {
+      commentContainerRef.current.scrollTop = commentContainerRef.current.scrollHeight;
+    }
+  };
   const [value, setValue] = useState("");
-
-  const handleDrawerOpen = () => {
-    setIsDrawerOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setIsDrawerOpen(false);
-  };
-
-  const handleChange = (e) => {
+  const handelChange = (e) => {
     setValue(e);
   };
-
+  const handleChange = (content,delta, source, editor) => {
+    // Handle the change event
+  };
   return (
-<Fragment>
-  <Breadcrumbs mainTitle="Inquiries" parent="Deals Queries" title="Master City" />
-
-  <Row>
-    <Col xl="12" className="box-col-8"> 
-      <div className="comment-box">
-        <Comments />
-      </div>
-    </Col>
-    {/* <Col xl="4"> */}
-      <Send/>
-    {/* </Col> */}
-  </Row>
-</Fragment>
-
+    <div style={{overflow: "hidden"}}> {/* Add this container */}
+      <Fragment>
+        <Breadcrumbs
+          mainTitle="Inquiries"
+          parent="Deals Queries"
+          title="Master City"
+        />
+        <Row>
+          <Col xl="12" className="box-col-8 xl-60">
+            <div className="comment-box" ref={commentContainerRef}>
+              <Comments />
+            </div>
+          </Col>
+        </Row>
+        <Row className="pt-4">
+          <Col>
+            <div>
+              <ReactQuill
+                id="editor_container"
+                theme="snow"
+                modules={{
+                  toolbar: [
+                    [{ 'header': [1, 2, false] }],
+                    ['bold', 'italic', 'underline'],
+                    ['image', 'code-block'],
+                  ],
+                }}
+                formats={['header', 'bold', 'italic', 'underline', 'image', 'code-block']}
+                value={''} // Pass the initial value here
+                onChange={handleChange}
+                style={{ height: '200px' }} // Adjust the height as needed
+              />
+            </div>
+          </Col>
+        </Row>
+        <Btn
+          attrBtn={{ color: "success", className: "m-r-15", type: "submit" }}
+        >
+          Send
+        </Btn>
+   
+    </Fragment>
+    </div>
   );
 };
 
 export default Queries;
 
+
+
 const Comments = () => {
+  const commentContainerRef = useRef(null);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, []);
+
+  const scrollToBottom = () => {
+    if (commentContainerRef.current) {
+      commentContainerRef.current.scrollTop =
+        commentContainerRef.current.scrollHeight;
+    }
+  };
 
 
   return (
-    <Fragment>
-     <div style={{ maxHeight: "450px", overflowY: "auto" }}> {/* Set max height and enable vertical scrolling */}
-        <UL attrUL={{ className: "simple-list" }}>
-        <LI attrLI={{ className: "border-0  bg-transparent" }}>
+    <div
+      ref={commentContainerRef}
+      style={{ maxHeight: '40vh', overflowY: 'auto', overflowX: 'hidden' }} // Set max height and add vertical scrollbar only
+    >
+      <UL attrUL={{ className: "simple-list" }}>
+        <LI attrLI={{ className: "border-0 bg-transparent" }}>
           <Media className="align-self-center">
             <Image
               attrImage={{
@@ -90,13 +126,10 @@ const Comments = () => {
                   "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
                 }
               </P>
-              {/* <Button color="primary" onClick={toggleModal}>
-                Give Feedback
-              </Button> */}
             </Media>
           </Media>
         </LI>
-        <LI attrLI={{ className: "border-0  bg-transparent" }}>
+        <LI attrLI={{ className: "border-0 bg-transparent" }}>
           <UL>
             <LI attrLI={{ className: "border-0 " }}>
               <Media>
@@ -123,7 +156,7 @@ const Comments = () => {
             </LI>
           </UL>
         </LI>
-        <LI attrLI={{ className: "border-0  bg-transparent" }}>
+        <LI attrLI={{ className: "border-0 bg-transparent" }}>
           <Media className="align-self-center">
             <Image
               attrImage={{
@@ -146,13 +179,10 @@ const Comments = () => {
                   "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
                 }
               </P>
-              {/* <Button color="primary" onClick={toggleModal}>
-                Give Feedback
-              </Button> */}
             </Media>
           </Media>
         </LI>
-        <LI attrLI={{ className: "border-0  bg-transparent" }}>
+        <LI attrLI={{ className: "border-0 bg-transparent" }}>
           <UL>
             <LI attrLI={{ className: "border-0 " }}>
               <Media>
@@ -179,7 +209,7 @@ const Comments = () => {
             </LI>
           </UL>
         </LI>
-        <LI attrLI={{ className: "border-0  bg-transparent" }}>
+        <LI attrLI={{ className: "border-0 bg-transparent" }}>
           <Media className="align-self-center">
             <Image
               attrImage={{
@@ -202,13 +232,10 @@ const Comments = () => {
                   "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
                 }
               </P>
-              {/* <Button color="primary" onClick={toggleModal}>
-                Give Feedback
-              </Button> */}
             </Media>
           </Media>
         </LI>
-        <LI attrLI={{ className: "border-0  bg-transparent" }}>
+        <LI attrLI={{ className: "border-0 bg-transparent" }}>
           <UL>
             <LI attrLI={{ className: "border-0 " }}>
               <Media>
@@ -235,41 +262,61 @@ const Comments = () => {
             </LI>
           </UL>
         </LI>
-       
-   
-    
+        <LI attrLI={{ className: "border-0 bg-transparent" }}>
+          <Media className="align-self-center">
+            <Image
+              attrImage={{
+                className: "align-self-center",
+                src: `${require("../../../assets/images/user/1.jpg")}`,
+                alt: "",
+              }}
+            />
+            <Media body>
+              <Row>
+                <Col md="4" className="xl-100">
+                  <H6 attrH6={{ className: "mt-0" }}>
+                    {"JolioMark"}
+                    <span> {"( Designer )"}</span>
+                  </H6>
+                </Col>
+              </Row>
+              <P>
+                {
+                  "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
+                }
+              </P>
+            </Media>
+          </Media>
+        </LI>
+        <LI attrLI={{ className: "border-0 bg-transparent" }}>
+          <UL>
+            <LI attrLI={{ className: "border-0 " }}>
+              <Media>
+                <Image
+                  attrImage={{
+                    className: "align-self-center",
+                    src: `${require("../../../assets/images/blog/9.jpg")}`,
+                    alt: "",
+                  }}
+                />
+                <Media body>
+                  <Row>
+                    <Col xl="12">
+                      <H6 attrH6={{ className: "mt-0" }}>{"Admin"}</H6>
+                    </Col>
+                  </Row>
+                  <P>
+                    {
+                      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
+                    }
+                  </P>
+                </Media>
+              </Media>
+            </LI>
+          </UL>
+        </LI>
       </UL>
-      </div>
-
-    </Fragment>
-  );
-};
-const Send = () => {
-  return (
-   <div className="chat-message clearfix">
-      <Row>
-        <Col xl="12" className="d-flex">
-          <div style={{ position: "relative", width: "100%" }}>
-            <div style={{ position: "absolute", bottom: 0, width: "100%" }}>
-              <InputGroup className="text-box">
-                <textarea
-                  className="form-control input-txt-bx"
-                  placeholder="Type a message......"
-                  rows={4} // Adjust the number of rows as needed
-                />
-                <Btn
-                  attrBtn={{
-                    color: "primary"
-                    // , onClick: () => handleMessagePress('send')
-                  }}
-                >
-                  Send
-                </Btn>
-              </InputGroup>
-            </div>
-          </div>
-        </Col>
-      </Row>
     </div>
   );
 };
+
