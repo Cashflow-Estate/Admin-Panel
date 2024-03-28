8595a178d3ec2b85dd3b1bd681c2d532d7de52cb
Syed M.Abid Zaidi <zaidiabid51472@gmail.com>
2024-03-29 00:12:05 +0500

    Changes

--- src/myPages/DealsSection/comp/ViewMore.jsx
+++ src/myPages/DealsSection/comp/ViewMore.jsx
@@ -9,41 +9,74 @@ import { useNavigate, useParams } from "react-router-dom";
 import { BiBed, BiBath, BiArea } from "react-icons/bi";
 import { CgUnavailable } from "react-icons/cg";
 import { Link } from "react-router-dom";
-import { Container, Row, Col, Badge } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import imageLg from "../../../../src/assets/cashflowimg/house-banner.png";
 import { Breadcrumbs, H3, Image, LI, P, UL } from "../../../AbstractElements";
-import { Card, CardBody, Media } from "reactstrap";
+import { Card, CardBody, Media, Nav, NavItem, NavLink } from "reactstrap";
 import { Availability, Brand } from "../../../Constant";
 import CustomizerContext from "../../../_helper/Customizer";
 import ProductContext from "../../../_helper/Ecommerce/Product";
 import Slider from "react-slick";
 import iimmgg from "../../../assets/cashflowimg/apartments/a1lg.png";
+import video from "../../../assets/CashflowLogos/video.mp4";
 
 const ViewMore = () => {
   const { id } = useParams();
   console.log(id);
 
+  const [activeSlider, setActiveSlider] = useState("image");
+
+  const toggleSlider = () => {
+    setActiveSlider(activeSlider === "image" ? "video" : "image");
+  };
+
   return (
     <Container fluid={true}>
       <div>
-        <Breadcrumbs
-          mainTitle="Details"
-          parent="Deals"
-          title="1"
-        />
+        <Breadcrumbs mainTitle="Details" parent="Deals" title="1" />
 
         <Row className="product-page-main p-0">
           <Col xxl="7" md="6">
             <Card>
+            <Nav tabs style={{ backgroundColor: "black", borderRadius: "15px", padding: "5px", marginBottom: "25px" }}>
+      <NavItem>
+        <NavLink
+          style={{
+            backgroundColor: activeSlider === "image" ? "green" : "black",
+            color: activeSlider === "image" ? "white" : "white",
+            borderRadius: "15px",
+            padding: "10px",
+            marginRight: "5px"
+          }}
+          onClick={() => setActiveSlider("image")}
+        >
+          Image
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink
+          style={{
+            backgroundColor: activeSlider === "video" ? "green" : "black",
+            color: activeSlider === "video" ? "white" : "white",
+            borderRadius: "15px",
+            padding: "10px",
+            marginRight: "5px"
+          }}
+          onClick={() => setActiveSlider("video")}
+        >
+          Video
+        </NavLink>
+      </NavItem>
+    </Nav>
               <CardBody>
-                {/* <Row> */}
-                <ImageSlider />
-                {/* </Row> */}
+                {activeSlider === "image" && <ImageSlider />}
+                {activeSlider === "video" && <VideoSlider />}
               </CardBody>
             </Card>
           </Col>
           <Col xxl="5" md="6" className="box-col-6">
             <DealsDetails />
+            
           </Col>
         </Row>
       </div>
@@ -97,7 +130,7 @@ const DealsDetails = () => {
                       <div className="d-flex ">
                         <BiBed
                           style={{
-                            color: "blue",
+                            color: "orange",
                             fontSize: "24px",
                           }}
                         />
@@ -113,7 +146,7 @@ const DealsDetails = () => {
                       <div className="d-flex ">
                         <BiBath
                           style={{
-                            color: "blue",
+                            color: "orange",
                             fontSize: "24px",
                           }}
                         />
@@ -129,7 +162,7 @@ const DealsDetails = () => {
                       <div className="d-flex ">
                         <BiArea
                           style={{
-                            color: "blue",
+                            color: "orange",
                             fontSize: "24px",
                           }}
                         />
@@ -145,7 +178,7 @@ const DealsDetails = () => {
                       <div className="d-flex ">
                         <CgUnavailable
                           style={{
-                            color: "blue",
+                            color: "orange",
                             fontSize: "24px",
                           }}
                         />
@@ -170,6 +203,62 @@ const DealsDetails = () => {
   );
 };
 
+const VideoSlider = () => {
+  const { productItem } = useContext(ProductContext);
+  const [state, setState] = useState({ nav1: null, nav2: null });
+  const slider1 = useRef();
+  const slider2 = useRef();
+  useEffect(() => {
+    setState({
+      nav1: slider1.current,
+      nav2: slider2.current,
+    });
+  }, []);
+  const { nav1, nav2 } = state;
+
+  return (
+    <Fragment>
+      <Slider
+        asNavFor={nav2}
+        className="product-slider"
+        arrows={false}
+        ref={(slider) => (slider1.current = slider)}
+      >
+        {productItem ? (
+          productItem.map((item) => {
+            return item.variants.map((items, id) => (
+              <video controls>
+              <source src={video} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+            ));
+          })
+        ) : (
+          <Media src={productItem.img} alt="" className="img-fluid" />
+        )}
+      </Slider>
+      <Slider
+        asNavFor={nav1}
+        ref={(slider) => (slider2.current = slider)}
+        slidesToShow={4}
+        swipeToSlide={true}
+        focusOnSelect={true}
+        infinite={true}
+        className="small-slick"
+      >
+        {productItem &&
+          productItem.map((item) => {
+            return item.variants.map((items, id) => (
+              <video controls>
+              <source src={video} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+            ));
+          })}
+      </Slider>
+    </Fragment>
+  );
+};
 const ImageSlider = () => {
   const { productItem } = useContext(ProductContext);
   const [state, setState] = useState({ nav1: null, nav2: null });
@@ -194,14 +283,15 @@ const ImageSlider = () => {
         {productItem ? (
           productItem.map((item) => {
             return item.variants.map((items, id) => (
-              <Image
-                attrImage={{
-                  src: `${iimmgg}`,
-                  alt: "",
-                  className: "img-fluid w-100",
-                }}
-                key={id}
-              />
+              <><Image
+              attrImage={{
+                src: `${iimmgg}`,
+                alt: "",
+                className: "img-fluid w-100",
+              }}
+              key={id}
+            />
+            </>
             ));
           })
         ) : (
