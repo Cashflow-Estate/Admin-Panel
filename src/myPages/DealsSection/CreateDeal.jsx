52f0ba609b718cb41d3995d3f73a3c5026c21172
Syed M.Abid Zaidi <zaidiabid51472@gmail.com>
2024-05-09 19:01:08 +0500

    New

--- src/myPages/DealsSection/CreateDeal.jsx
+++ src/myPages/DealsSection/CreateDeal.jsx
@@ -2,7 +2,7 @@ import Dropzone from "react-dropzone";
 import React, { Fragment, useEffect, useState } from "react";
 import { Breadcrumbs, Btn, H6 } from "../../AbstractElements";
 import { useForm, Controller } from "react-hook-form";
-import { Spinner } from "react-bootstrap";
+import { Dropdown, FormLabel, Spinner } from "react-bootstrap";
 
 import {
   Container,
@@ -25,7 +25,7 @@ import { toast } from "react-toastify";
 import { useNavigate, useParams } from "react-router";
 import ReactGoogleAutocomplete, {
   Autocomplete,
-} from "react-google-autocomplete"; 
+} from "react-google-autocomplete";
 
 const CreateDeal = () => {
   const [selectedAddress, setSelectedAddress] = useState("");
@@ -46,7 +46,11 @@ const CreateDeal = () => {
   const { id } = useParams();
 
   const [dealData, setDealData] = useState(null);
+  const [propertyType, setPropertyType] = useState(""); // State to hold selected property type
 
+  const handlePropertyTypeChange = (selectedType) => {
+    setPropertyType(selectedType);
+  };
   useEffect(() => {
     const fetchDealById = async () => {
       try {
@@ -67,6 +71,7 @@ const CreateDeal = () => {
 
   const formData = watch();
   const [images, setImages] = useState([]);
+  const [featureimage, setFeatureImages] = useState([]);
   const [files, setFiles] = useState([]);
 
   useEffect(() => {
@@ -97,19 +102,12 @@ const CreateDeal = () => {
   const [client, setClient] = useState(false);
   const [formSubmitted, setFormSubmitted] = useState(false);
   const [selectedOptions, setSelectedOptions] = useState([]);
+  console.log("🚀 ~ CreateDeal ~ selectedOptions:", selectedOptions);
   let opt = selectedOptions.map((val) => val.label);
 
   const [useEmail, setUseEmail] = useState(false);
   const [email, setEmail] = useState("");
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-    if (event.target.value?.trim() !== "") {
-      setUseEmail(true);
-    } else {
-      setUseEmail(false);
-    }
-  };
   const history = useNavigate();
 
   const [addressOptions, setAddressOptions] = useState([]);
@@ -167,6 +165,7 @@ const CreateDeal = () => {
       setApproxAnnualMinReturn(dealData.data.annually_return_min || "");
       setApproxAnnualMaxReturn(dealData.data.annually_return_max || "");
 
+      setFeatureImages(dealData?.data?.featureimage);
       setImages(dealData.data.images);
       setMonthlyCashMin(dealData.data.monthly_cash_min);
       setMonthlyCashMax(dealData.data.monthly_cash_max);
@@ -275,6 +274,42 @@ const CreateDeal = () => {
       handleAddressChange(debouncedAddressSearch);
     }
   }, [debouncedAddressSearch]);
+  const [sendByEmail, setSendByEmail] = useState(false);
+  const [sendToALLCheckbox, setSendToALLCheckbox] = useState(false);
+  const [sendToSpecificExistingCustomer, setSendToSpecificExistingCustomer] =
+    useState(false);
+  const handleSendByEmailToggle = () => {
+    setSendByEmail(!sendByEmail);
+  };
+  const handleSendToALLCheckbox = () => {
+    setSendToALLCheckbox(!sendToALLCheckbox);
+  };
+  const handleSpecificExistingCustomer = () => {
+    setSendToSpecificExistingCustomer(!sendToSpecificExistingCustomer);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const onSubmit = async (data) => {
+    setFormSubmitted(true);
+    try {
+      // Submit logic
+    } catch (error) {
+      toast.error("Failed to create/update deal.");
+    } finally {
+      setFormSubmitted(false);
+    }
+  };
+
+  // Validation function to ensure either "Send To" or email is provided
+  const validateSendOptions = () => {
+    if (!sendByEmail && selectedOptions.length === 0 && !email) {
+      return "Please select either 'Send To' or provide an email";
+    }
+    return true;
+  };
   return (
     <Fragment>
       <Breadcrumbs
@@ -379,7 +414,34 @@ const CreateDeal = () => {
                           </span>
                         </FormGroup>
                       </Col>
+                      <Col sm="4">
+                        <FormGroup>
+                          <H6>{"Property Type"}</H6>
+                          <Dropdown onSelect={handlePropertyTypeChange}>
+                            <Dropdown.Toggle variant="" id="dropdown-basic">
+                              {propertyType
+                                ? propertyType
+                                : "Select Property Type"}
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                              <Dropdown.Item eventKey="Apartment">
+                                Apartment
+                              </Dropdown.Item>
+                              <Dropdown.Item eventKey="Single Family Home">
+                                Single Family Home
+                              </Dropdown.Item>
+                              <Dropdown.Item eventKey="Condo">
+                                Condo
+                              </Dropdown.Item>
+                              <Dropdown.Item eventKey="Town Home">
+                                Town Home
+                              </Dropdown.Item>
+                            </Dropdown.Menu>
+                          </Dropdown>
+                        </FormGroup>
+                      </Col>
                     </Row>
+
                     <Row>
                       <Col sm="4">
                         <FormGroup>
@@ -466,6 +528,20 @@ const CreateDeal = () => {
                       </Col>
                       <Row>
                         <Col sm="12">
+                          <SingleDropzone
+                            images={featureimage}
+                            setImages={setFeatureImages}
+                            dealData={dealData}
+                          />
+                        </Col>
+                        {images.length === 0 && (
+                          <p style={{ color: "red" }}>
+                            Please upload feature image.
+                          </p>
+                        )}
+                      </Row>
+                      <Row>
+                        <Col sm="12">
                           <FormGroup>
                             <h6 style={{ color: "black" }}>Address</h6>
                             {id ? (
@@ -503,85 +579,74 @@ const CreateDeal = () => {
                           </FormGroup>
                         </Col>
                       </Row>
-
-                      <Col sm="12">
-                        <FormGroup>
-                          <Label for="sendTo">Send To:</Label>
-                          <Controller
-                            name="sendTo"
-                            control={control}
-                            rules={{
-                              validate: () => {
-                                if (
-                                  !useEmail &&
-                                  !selectedOptions.length &&
-                                  !email.length > 0
-                                ) {
-                                  return "Please select either 'Send To' or provide an email";
-                                }
-                                return true;
-                              },
+                    </Row>
+                    <input
+                      type="checkbox"
+                      id="sendToALLCheckbox"
+                      checked={sendToALLCheckbox}
+                      onChange={handleSendToALLCheckbox}
+                    />
+                    <label htmlFor="
+                    sendToALLCheckbox
+                    sendToALLCheckbox">
+                      Send to specific customer type
+                    </label>
+                    {sendToALLCheckbox && (
+                      <Controller
+                        name="sendTo"
+                        control={control}
+                        rules={{ validate: validateSendOptions }}
+                        render={({ field }) => (
+                          <Select
+                            {...field}
+                            options={options}
+                            isMulti
+                            onChange={(value) => {
+                              field.onChange(value);
+                              setSelectedOptions(value);
                             }}
-                            render={({ field }) => (
-                              <Select
-                                {...field}
-                                options={options}
-                                isMulti
-                                onChange={(value) => {
-                                  field.onChange(value);
-                                  setSelectedOptions(value);
-                                  setUseEmail(false);
-                                }}
-                                isDisabled={useEmail}
-                              />
-                            )}
                           />
-
-                          {errors.sendTo && (
-                            <span className="text-danger">
-                              {errors.sendTo.message}
-                            </span>
-                          )}
-                        </FormGroup>
-                      </Col>
-                    </Row>
-
-                    <Row>
-                      <Col sm="6">
-                        <FormGroup>
-                          <h6 style={{ color: "black" }}>OR Send By Email:</h6>
-                          <input
-                            className="form-control"
-                            type="email"
-                            name="sendByEmail"
-                            id="sendByEmail"
-                            placeholder="Enter email"
-                            disabled={selectedOptions?.length}
-                            onChange={handleEmailChange}
-                            value={email}
-                          />
-
-                          {errors.sendByEmail && (
-                            <span className="text-danger">
-                              {errors.sendByEmail.message}
-                            </span>
-                          )}
-                        </FormGroup>
-                      </Col>
-
-                      <Col sm="12">
-                        <MultiDropzone
-                          images={images}
-                          setImages={setImages}
-                          dealData={dealData}
-                        />
-                      </Col>
-                      {images.length === 0 && (
-                        <p style={{ color: "red" }}>
-                          Please upload at least one image.
-                        </p>
-                      )}
-                    </Row>
+                        )}
+                      />
+                    )}
+                    <br></br>
+                    <input
+                      type="checkbox"
+                      id="sendByEmailCheckbox"
+                      checked={sendByEmail}
+                      onChange={handleSendByEmailToggle}
+                    />
+                    <label htmlFor="sendByEmailCheckbox">
+                      Send to a new customer
+                    </label>
+                    {sendByEmail && (
+                      <input
+                        className="form-control"
+                        type="email"
+                        name="sendByEmail"
+                        placeholder="Enter email"
+                        onChange={handleEmailChange}
+                        value={email}
+                      />
+                    )}
+                    <br></br>
+                    <input
+                      type="checkbox"
+                      id="sendToSpecificExistingCustomer"
+                      checked={sendToSpecificExistingCustomer}
+                      onChange={handleSpecificExistingCustomer}
+                    />
+                    <label htmlFor="sendToSpecificExistingCustomer">
+                      Send To Specific Existing Customer
+                    </label>
+                    {sendToSpecificExistingCustomer && (
+                      <>
+                        {" "}
+                     
+                        <CustomSelect  name="sendToSpecificExistingCustomer"/>
+                      </>
+                    )}
+           
                     <Row>
                       <Col>
                         <div className="text-end">
@@ -620,6 +685,7 @@ const CreateDeal = () => {
 };
 
 export default CreateDeal;
+
 const MultiDropzone = ({ setImages, dealData, images }) => {
   const handleDrop = (acceptedFiles) => {
     setImages((prevImages) => [...prevImages, ...acceptedFiles]);
@@ -705,3 +771,141 @@ const MultiDropzone = ({ setImages, dealData, images }) => {
     </div>
   );
 };
+const SingleDropzone = ({ setImages, dealData, images }) => {
+  const handleDrop = (acceptedFiles) => {
+    // Only allow one image to be selected for feature image
+    const selectedImage = acceptedFiles[0];
+    setImages([selectedImage]);
+  };
+
+  const handleDelete = () => {
+    setImages([]);
+  };
+
+  return (
+    <div className="App2">
+      <Dropzone onDrop={handleDrop} accept="image/*" multiple={false}>
+        {({ getRootProps, getInputProps }) => (
+          <div
+            className="dropzone"
+            {...getRootProps()}
+            style={{ border: `2px dashed var(--theme-deafult)` }}
+          >
+            <input {...getInputProps()} />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fill="green"
+              className="bi bi-cloud-upload"
+              viewBox="0 0 16 16"
+              style={{ color: `var(--theme-secondary)` }}
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383"
+              />
+              <path
+                fillRule="evenodd"
+                d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708z"
+              />
+            </svg>
+            <p style={{ color: `var(--theme-deafult)` }}>
+              Drag & drop an image here, or click to select a file
+            </p>
+          </div>
+        )}
+      </Dropzone>
+      <div className="image-preview">
+        {images.length > 0 && (
+          <div className="image-container">
+            <img src={URL.createObjectURL(images[0])} alt="Feature Image" />
+            <div className="image-delete" onClick={handleDelete}>
+              <FaTrash />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const CustomSelect = () => {
+  const [selectedOptions, setSelectedOptions] = useState([]);
+
+  const handleChange = (selectedValues) => {
+    setSelectedOptions(selectedValues);
+  };
+
+  const options = [
+    { value: '1', label: 'John Doe || john@example.com', color: 'green' },
+    { value: '2', label: 'Jane Smith || jane@example.com', color: 'red' },
+    { value: '3', label: 'Michael Johnson || michael@example.com', color: 'green' },
+    // Add more options as needed
+  ];
+
+  return (
+    <div>
+      <Select
+        isMulti
+        options={options}
+        value={selectedOptions}
+        onChange={handleChange}
+        placeholder="Search..."
+      />
+    </div>
+  );
+};
+
+// const CustomSelect = () => {
+//   const [selectedOptions, setSelectedOptions] = useState([]);
+
+//   const handleChange = (selectedValues) => {
+//     setSelectedOptions(selectedValues);
+//   };
+
+//   const options = [
+//     { value: '1', label: 'John Doe || john@example.com', color: 'green' },
+//     { value: '2', label: 'Jane Smith || jane@example.com', color: 'red' },
+//     { value: '3', label: 'Michael Johnson || michael@example.com', color: 'green' },
+//     // Add more options as needed
+//   ];
+
+//   const customStyles = {
+//     option: (provided, state) => ({
+//       ...provided,
+//       display: 'flex',
+//       alignItems: 'center',
+//     }),
+//     dot: (color) => ({
+//       height: 10,
+//       width: 10,
+//       borderRadius: '50%',
+//       backgroundColor: color,
+//       marginRight: 8,
+//     }),
+//   };
+
+//   const customOptionLabel = ({ label, color }) => (
+//     <div style={{ display: 'flex', alignItems: 'center' }}>
+//       <span style={customStyles.dot(color)}></span>
+//       {label}
+//     </div>
+//   );
+
+//   return (
+//     <div>
+//       <Select
+//         isMulti
+//         options={options}
+//         value={selectedOptions}
+//         onChange={handleChange}
+//         placeholder="Search..."
+//         styles={customStyles}
+//         getOptionLabel={customOptionLabel}
+//         getOptionValue={(option) => option.value}
+//         isSearchable={true} // Ensure search functionality is enabled
+//       />
+//     </div>
+//   );
+// };
